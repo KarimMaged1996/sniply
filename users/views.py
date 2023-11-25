e3ca627ffdf545c090382218ba31d4768c88c1e6
Karim Maged <karim.maged2020@yahoo.com>
2023-11-25 17:04:06 +0200

    Provide the base template with the user object

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from .forms import customUserCreationForm
+from .models import CustomUser
 
 
 # Create your views here.
@@ -8,3 +9,12 @@ class RegisterView(CreateView):
     form_class = customUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/register.html"
+
+
+class BaseTemplateView(DetailView):
+    model = CustomUser
+    context_object_name = "user"
+    template_name = "snippets/base.html"
+
+    def get_queryset(self):
+        return CustomUser.objects.get(pk=self.request.user.pk)
